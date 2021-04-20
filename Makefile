6282a00040f470e549e9f537c45d336c663f756f
Barry <chiroptical@gmail.com>
2021-04-20 15:44:15 -0400

    Render every constructor as a tree via TH

--- Makefile
+++ Makefile
@@ -1,7 +1,16 @@
 build:
 	stack build
 
+run:
+	stack run
+
 format:
 	find src/ test/ app/ -name "*.hs" -exec ormolu -i {} \;
 
-.PHONY: build format
+repl:
+	stack repl
+
+ghcid:
+	ghcid -c stack repl
+
+.PHONY: build run format repl ghcid
--- app/Main.hs
+++ app/Main.hs
@@ -8,10 +8,21 @@ import Language.Haskell.TH
 import Lib
 import Types
 
+$(genEnzymeInfo ''Int)
+
 main :: IO ()
 main = do
-  -- Printing the @DatatypeInfo@ for a type
-  print $(stringE . show =<< newtypeDatatypeInfo)
+  -- -- Printing the @DatatypeInfo@ for a type
+  -- print $(stringE . show =<< newtypeDatatypeInfo)
 
-  -- Use the @ToEnzymeStringInstance@ to print the name of the dataconstructor
-  print (toEnzymeStringInstance $ Proxy @Newtype)
+  -- -- Use the @ToEnzymeStringInstance@ to print the name of the dataconstructor
+  -- print (toEnzymeStringInstance $ Proxy @Newtype)
+
+  -- -- Use the @ToEnzymeInfo@ to print the info of the dataconstructor
+  -- print (toEnzymeDatatypeInfo $ Proxy @SumOfProducts)
+
+  -- -- Use the @ToEnzymeInfo@ to print the info of the dataconstructor
+  -- print (toEnzymeInfo $ Proxy @SumOfProducts)
+
+  -- Use the @ToEnzymeRecurseToPrimitives@ to print the info of the dataconstructor
+  putStrLn (toEnzymeRecurseToPrimitives $ Proxy @SumOfProducts)
